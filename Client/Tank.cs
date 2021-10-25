df9a0f4d1c5bad32f4a6b45393b724a3ec80025c
User <User@MARGOPC>
2021-10-25 13:47:35 +0300

    Add project files.

---
+++ Client/Tank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Client
+{
+    public class Tank
+    {
+        public int CoordX { get; set; }
+        public int CoordY { get; set; }
+        public int Speed { get; set; }
+        public int[] Color { get; set; }
+        public float Rotation { get; set; }
+        public Tank()
+        {
+            Random rand = new Random();
+            CoordX = 300;
+            CoordY = 300;
+            Speed = 3;
+            Color = new int[] { rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255) };
+            Rotation = 0f;
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static ServerData serverData = new ServerData();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start server...");
+            try
+            {
+                serverData.socket.Bind(serverData.iPEndPoint);
+                serverData.socket.Listen(10);
+
+                Task.Factory.StartNew(() => Connect());
+            }
+            catch (Exception ex) { }
+
+            Console.ReadLine();
+        }
+        static void Connect()
+        {
+            while (true)
+            {
+                serverData.socketClient = serverData.socket.Accept();
+                serverData.socketClientsList.Add(serverData.socketClient);
+            }
+        }
+    }
+}
